d7e560bbf4c03fbeb9632265b7482226d05f767c
Andrea Baldonado <148048330+iyanegha@users.noreply.github.com>
2024-05-13 08:01:08 +0000

    Initial commit

---
+++ conf.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+// connessione al DB
+module.exports = {
+   "host": "mysqlbaldonado-progtpsiandrea.c.aivencloud.com",
+   "user": "avnadmin",
+   "password": "AVNS_2Ocn2UWOIPeeTblPYnJ",
+   "database": "defaultdb",
+    "port": 22834,
+  "ssl": {
+    "ca" : fs.readFileSync('ca.pem'),
+    "rejectUnauthorized": true
+  }
+}
